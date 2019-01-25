f03b6f744ea6e93c2f73b040da856e765064a45d
Jaroslaw Kisiolek <kisiolek.jaroslaw@gmail.com>
2019-01-26 00:11:16 +0100

    Add witch basic movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,23 +2,37 @@
 
 public class Player : MonoBehaviour
 {
+    Animator m_Animator;
+    private void Start()
+    {
+        m_Animator = gameObject.GetComponent<Animator>();
+    }
+
     void Update()
     {
         if(Input.GetKey("right"))
         {
             transform.Translate(Vector3.right * Time.deltaTime);
+            m_Animator.SetTrigger("move_right");
         }
         else if (Input.GetKey("left"))
         {
             transform.Translate(Vector3.left * Time.deltaTime);
+            m_Animator.SetTrigger("move_left");
         }
         else if(Input.GetKey("up"))
         {
             transform.Translate(Vector3.up * Time.deltaTime);
+            m_Animator.SetTrigger("move_up");
         }
         else if (Input.GetKey("down"))
         {
             transform.Translate(Vector3.down * Time.deltaTime);
+            m_Animator.SetTrigger("move_down");
+        }
+        else
+        {
+            m_Animator.SetTrigger("idle");
         }
     }
 }
