bd3b2dd714bff657bf677d91ad389e86d8dd4ce6
Wozniaxos <kacperwozniax@gmail.com>
2019-01-25 21:39:14 +0100

    Add movement and split cameras

---
+++ Assets/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetKey("right"))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime);
+        }
+        if (Input.GetKey("left"))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime);
+        }
+
+        if (Input.GetKey("up"))
+        {
+            transform.Translate(Vector3.up * Time.deltaTime);
+        }
+        if (Input.GetKey("down"))
+        {
+            transform.Translate(Vector3.down * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Player2.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Player2 : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime);
+        }
+
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(Vector3.up * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(Vector3.down * Time.deltaTime);
+        }
+    }
+}
