343318bffe9f82e29062fa761eb506e974d9e36b
Wozniaxos <kacperwozniax@gmail.com>
2019-01-26 14:48:08 +0100

    Add boster

---
+++ Assets/Booster.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Application
+{
+    public class Booster
+    {
+        public bool boostStarted = false;
+        public float boost = 1;
+        public float speed;
+        float initialSpeed;
+
+        public Booster(float initialSpeed)
+        {
+            this.initialSpeed = initialSpeed;
+        }
+        public void initBoost(float fromSpeed)
+        {
+            boostStarted = true;
+            this.speed = fromSpeed;
+            speed += boost;
+        }
+        public float decreaseToBoostEnd()
+        {
+            if (speed <= initialSpeed)
+            {
+                boostStarted = false;
+                speed = initialSpeed;
+                return speed;
+            }
+            else
+            {
+                speed -= (Time.deltaTime * 0.3f);
+                return speed;
+            }
+        }
+    }
+}
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,5 +1,6 @@
-﻿using UnityEngine;
-using System.Threading.Tasks;
+﻿using Application;
+using UnityEngine;
+
 
 public class Player : MonoBehaviour
 {
@@ -10,16 +11,26 @@ public class Player : MonoBehaviour
 
     Direction direction;
     RaycastHit2D hitter;
-    RaycastHit2D hitter2;
-   
+
+    public static float movementSpeed = 1;
+    public float boostValue = 1;
     public Vector3 offset = new Vector3(0, -0.51f, 0);
     public Vector3 bottomRayOffset = new Vector3(0f, -0.72f, 0);
     public Vector3 prevMoveBy = new Vector3(0, 0, 0);
+    Booster booster = new Booster(movementSpeed);
 
     private void Start()
     {
         m_Animator = gameObject.GetComponent<Animator>();
         direction = Direction.NONE;
+    }
+
+    private void decreaseIfBoost()
+    {  
+        if (booster.boostStarted)
+        {
+            movementSpeed = booster.decreaseToBoostEnd();
+        }
     }
 
     void Update()
@@ -53,71 +64,25 @@ public class Player : MonoBehaviour
         }
 
         prevMoveBy = moveBy;
-
-
-        //if (Input.GetKey("left"))
-        //{
-        //    moveBy = Vector3.left;
-        //    direction = Direction.LEFT;
-        //}
-        //else if(Input.GetKey("up"))
-        //{
-        //    moveBy = Vector3.up;
-        //    direction = Direction.UP;
-        //}
-        //else if (Input.GetKey("down"))
-        //{
-        //    moveBy = Vector3.down;
-        //    direction = Direction.DOWN;
-        //}
-        //else
-        //{
-        //    direction = Direction.NONE;
-        //    moveBy = new Vector3(0, 0, 0);
-        //}
         hitter = Physics2D.Raycast(transform.position + bottomRayOffset, moveBy, 0.2f);
-        //if (previousDirection != direction)
-        //{
-        //    switch (direction)
-        //    {
-        //        case Direction.DOWN:
-        //            m_Animator.SetTrigger("move_down");
-        //            break;
-        //        case Direction.UP:
-        //            m_Animator.SetTrigger("move_up");
-        //            break;
-        //        case Direction.LEFT:
-        //            m_Animator.SetTrigger("move_left");
-        //            break;
-        //        case Direction.RIGHT:
-        //            m_Animator.SetTrigger("move_right");
-        //            break;
-        //        case Direction.NONE:
-        //            m_Animator.SetTrigger("idle");
-        //            break;
-        //    }
-        //}
         if (hitter.collider != null)
         {
             if (hitter.collider.tag == "booster")
             {
-                speed = 2;
-                Task.Delay(2000).ContinueWith((t) =>
+                if (!booster.boostStarted)
                 {
-                    speed = 1;
-                });
+                    booster.initBoost(movementSpeed);
+                }
             }
             else
             {
                 moveBy = new Vector3(0, 0, 0);
-                //Display the point in world space where the ray hit the collider's surface.
                 Debug.DrawRay(transform.position + bottomRayOffset, transform.TransformDirection(Vector3.right) * hitter.distance, Color.yellow, 5);
-                Debug.Log(hitter.collider.tag);
-                Debug.Log("HIT");
             }
            
         }
-        transform.Translate(moveBy * speed * Time.deltaTime);
+        transform.Translate(moveBy * movementSpeed * Time.deltaTime);
+        decreaseIfBoost();
     }
 
     void OnDrawGizmos()
