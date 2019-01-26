2dd6068272e692f5943993a4324af11f69bedce5
Jaroslaw Kisiolek <kisiolek.jaroslaw@gmail.com>
2019-01-26 02:00:11 +0100

    Improve horizontal movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -3,36 +3,66 @@
 public class Player : MonoBehaviour
 {
     Animator m_Animator;
+
+    enum Direction { NONE, LEFT, UP, RIGHT, DOWN };
+
+    Direction direction;
+
     private void Start()
     {
         m_Animator = gameObject.GetComponent<Animator>();
+        direction = Direction.NONE;
     }
 
     void Update()
     {
+        Direction previousDirection = direction;
+
         if(Input.GetKey("right"))
         {
             transform.Translate(Vector3.right * Time.deltaTime);
-            m_Animator.SetTrigger("move_right");
+            direction = Direction.RIGHT;
         }
         else if (Input.GetKey("left"))
         {
             transform.Translate(Vector3.left * Time.deltaTime);
-            m_Animator.SetTrigger("move_left");
+            direction = Direction.LEFT;
         }
         else if(Input.GetKey("up"))
         {
             transform.Translate(Vector3.up * Time.deltaTime);
-            m_Animator.SetTrigger("move_up");
+            direction = Direction.UP;
         }
         else if (Input.GetKey("down"))
         {
             transform.Translate(Vector3.down * Time.deltaTime);
-            m_Animator.SetTrigger("move_down");
+            direction = Direction.DOWN;
         }
         else
         {
-            m_Animator.SetTrigger("idle");
+            direction = Direction.NONE;
+        }
+
+        if (previousDirection != direction)
+        {
+            switch(direction)
+            {
+                case Direction.DOWN:
+                    m_Animator.SetTrigger("move_down");
+                    break;
+                case Direction.UP:
+                    m_Animator.SetTrigger("move_up");
+                    break;
+                case Direction.LEFT:
+                    m_Animator.SetTrigger("move_left");
+                    break;
+                case Direction.RIGHT:
+                    m_Animator.SetTrigger("move_right");
+                    break;
+                case Direction.NONE:
+                    m_Animator.SetTrigger("idle");
+                    break;
+            }
         }
     }
 }
