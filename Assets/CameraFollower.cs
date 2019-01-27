e27f8ba0af8c35c472444d4b2d4fcab7df309dcf
Wozniaxos <kacperwozniax@gmail.com>
2019-01-27 03:42:18 +0100

    Camera follow

---
+++ Assets/CameraFollower.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    public GameObject player;
+    public float minBoundary = 0;
+    public float maxBoundary = 53;
+
+    void Update()
+    {
+        var playerDistance = Vector2.Distance(player.transform.position, transform.position);
+        var playerPos = player.transform.position;
+
+        var playerInMap = playerPos.x >= minBoundary && playerPos.x <= maxBoundary && playerPos.y >= minBoundary && playerPos.y <= maxBoundary;
+
+
+        if (playerDistance > 2.5 && playerInMap)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(playerPos.x, playerPos.y, -10), Time.deltaTime * 4);
+        }
+    }
+}
